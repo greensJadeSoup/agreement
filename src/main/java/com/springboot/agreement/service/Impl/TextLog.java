7e2b35e2d5d24baef867bfaafcb082c42c82d3ce
greensJadeSoup <1252091009@qq.com>
2019-09-18 14:21:56 +0800

    1.springboot使用log4j模板 2.按日期，大小分割日志文件 3.按日期生成文件夹，log4j动态设置路径

--- src/main/java/com/springboot/agreement/service/Impl/TextLog.java
+++ src/main/java/com/springboot/agreement/service/Impl/TextLog.java
@@ -1,10 +1,13 @@
 package com.springboot.agreement.service.Impl;
 
+import com.springboot.agreement.utils.log4j.DynamicBuildFolder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class TextLog {
     public static void main(String[] args) {
+        DynamicBuildFolder dynamicBuildFolder = new DynamicBuildFolder();
+        DynamicBuildFolder.storage();
         Log log = LogFactory.getLog(TextLog.class);
         for(int i=0;i<=500;i++){
             System.out.println("循环"+"--"+i);
